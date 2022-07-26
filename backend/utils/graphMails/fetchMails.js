7c449fd9601346e711af03050d01f2259424a1a5
Sanjay <sanzaikc@gmail.com>
2022-07-26 10:59:22 +0545

    mend

--- backend/utils/graphMails/fetchMails.js
+++ backend/utils/graphMails/fetchMails.js
@@ -2,12 +2,6 @@ import axios from "axios";
 
 import getToken from "./getAuthToken.js";
 
-const _userId = "93db7f63-122b-4c4b-b7f4-1c7170cbf664"; //user's id
-const _folderId =
-  "AQMkADMwNTY5MWY0LTVmM2EtNDczNC1hZjgxLWRhNjc2ZGU3ODA2NwAuAAADvwm4BnriAkaEj8ZjPdWH-AEA6beG6l4eqE6FxKAdpUagyQAAAgEMAAAA"; //user's mail folder id
-
-var messages = [];
-
 const getMessages = async (token, url) => {
   console.log("Fetching mails from:", url);
 
@@ -20,11 +14,6 @@ const getMessages = async (token, url) => {
     },
   });
 
-  // messages = [...messages, ...res?.data?.value];
-
-  // if (res.data[`@odata.nextLink`])
-  //   return getMessages(token, res.data[`@odata.nextLink`]);
-
   return res?.data.value;
 };
 
@@ -33,7 +22,7 @@ const getInbox = async () => {
 
   try {
     // fetching mails from user's "INBOX" mail folder
-    const url = `https://graph.microsoft.com/v1.0/users/${_userId}/mailFolders/${_folderId}/messages`;
+    const url = `https://graph.microsoft.com/v1.0/users/${process.env.USER_ID}/mailFolders/${process.env.MAIL_FOLDER_ID}/messages`;
 
     const messages = await getMessages(token, url);
 
