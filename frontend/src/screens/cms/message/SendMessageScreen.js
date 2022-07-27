09d930deca7e09f72aed59c11ff1c9f344d27d8c
Sanjay <sanzaikc@gmail.com>
2022-07-27 07:54:19 +0545

    mend

--- frontend/src/screens/cms/message/SendMessageScreen.js
+++ frontend/src/screens/cms/message/SendMessageScreen.js
@@ -3,6 +3,8 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import {
   Box,
+  Button,
+  FormControl,
   Grid,
   IconButton,
   Paper,
@@ -10,6 +12,7 @@ import {
   Typography,
 } from "@mui/material";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import SendIcon from "@mui/icons-material/Send";
 
 export const SendMessageScreen = () => {
   const navigate = useNavigate();
@@ -36,13 +39,56 @@ export const SendMessageScreen = () => {
       </Paper>
 
       <Paper elevation={0}>
-        <Box display="flex" alignItems="center" sx={{ p: 3 }}>
-          <Typography>To</Typography>
-          <TextField
-            placeholder="Enter email address"
-            size="small"
-            variant="standard"
-          />
+        <Box sx={{ p: 3 }}>
+          <Grid container spacing={4}>
+            <Grid item md={2}>
+              <Typography sx={{ color: "gray" }}>To</Typography>
+            </Grid>
+            <Grid item md={6}>
+              <FormControl fullWidth>
+                <TextField
+                  name="email"
+                  placeholder="Enter email address"
+                  size="small"
+                  variant="standard"
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+          <br />
+          <Grid container spacing={4}>
+            <Grid item md={2}>
+              <Typography sx={{ color: "gray" }}>Subject</Typography>
+            </Grid>
+            <Grid item md={6}>
+              <FormControl fullWidth>
+                <TextField
+                  name="subject"
+                  placeholder="Enter subject"
+                  size="small"
+                  variant="standard"
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+          <br />
+          <Grid item md={12}>
+            <Typography sx={{ color: "gray" }}>Message</Typography>
+            <Box
+              sx={{
+                p: 3,
+                marginTop: 1,
+                borderRadius: 2,
+                backgroundColor: "#fafafa",
+              }}
+            ></Box>
+          </Grid>
+          <br />
+          <Box display="flex" justifyContent="flex-end">
+            <Button variant="contained" endIcon={<SendIcon />}>
+              Send
+            </Button>
+          </Box>
         </Box>
       </Paper>
     </Box>
